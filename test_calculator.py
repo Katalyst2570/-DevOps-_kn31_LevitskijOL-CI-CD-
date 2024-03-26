ab9c8acd4c82e69030e3aada497c788a69c8c734
Katalyst2570 <116761241+Katalyst2570@users.noreply.github.com>
2024-03-26 11:04:35 +0200

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -13,3 +13,16 @@ class TestCalculator(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main()
 
+class TestCalculator2(unittest.TestCase):
+    def test_add(self):
+        """Перевіряє, чи працює функція додавання."""
+        self.assertEqual(add(3, 6), 9)
+
+    def test_subtract(self):
+        """Перевіряє, чи працює функція віднімання."""
+        self.assertEqual(subtract(5, 2), 3)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
